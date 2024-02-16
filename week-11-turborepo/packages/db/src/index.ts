8e9c18f571a3401e9c3587fbd2c1c36d6fe631e4
Danish-Belal <danishexplore019@gmail.com>
2024-02-16 17:28:35 +0530

    Database in packages

---
+++ week-11-turborepo/packages/db/src/index.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose"
+
+// Import all code of authentication
+const userSchema = new mongoose.Schema({
+     username : {type : String},
+     password : String,
+     purchasedCourse: [{type: mongoose.Schema.Types.ObjectId, ref: 'Course'}]
+
+});
+
+const adminSchema = new mongoose.Schema({
+     username : String,
+     password : String
+});
+
+const courseSchema = new mongoose.Schema({
+     title: String,
+     description: String,
+     price : Number,
+     imageLink : String,
+     published : Boolean
+});
+
+export const Admin = mongoose.model('Admin', adminSchema);
+export const User = mongoose.model('User' , userSchema);
+export const Course = mongoose.model('Course', courseSchema)
