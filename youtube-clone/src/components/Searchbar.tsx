d71c336b8b9ec5dd2c490ed6c49df82834463f4c
Danish-Belal <danishexplore019@gmail.com>
2024-02-26 21:33:55 +0530

    Searchborx

--- youtube-clone/src/components/Searchbar.tsx
+++ youtube-clone/src/components/Searchbar.tsx
@@ -1,7 +1,7 @@
 
 export function Searchbox(){
      return(
-          <div className=" w-64 grid grid-cols-2 border-2 border-gray-500 rounded-full">
+          <div className=" w-64 grid grid-cols-2  border-gray-500 rounded-full">
                <div className="">
                     <input type="text" placeholder="text here"></input>
                </div>
