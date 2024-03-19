da2b9b6287bb70e6ba0d7bfbaa527c0d44a68cc6
Danish-Belal <danishexplore019@gmail.com>
2024-03-20 02:31:27 +0530

    Client

---
+++ TRPC/client/index.ts
@@ -0,0 +1,23 @@
+import { createTRPCClient, httpBatchLink } from '@trpc/client';
+import type { AppRouter } from '../server/index';
+//     👆 **type-only** import
+
+// Pass AppRouter as generic here. 👇 This lets the `trpc` object know
+// what procedures are available on the server and their input/output types.
+const trpc = createTRPCClient<AppRouter>({
+  links: [
+    httpBatchLink({
+      url: 'http://localhost:3000',
+    }),
+  ],
+});
+
+async function main(){
+     const response = await trpc.createTodo.mutate({
+          title: "goto gym",
+          description: "Hit the gym daily"
+     })
+
+     console.log(response);
+}
+main();
