395080578675b460aa1d0aff0b665dc711ab4eaa
Danish-Belal <danishexplore019@gmail.com>
2024-03-22 22:10:49 +0530

    Context defined

--- TRPC/server/trpc.ts
+++ TRPC/server/trpc.ts
@@ -1,10 +1,12 @@
 import { initTRPC } from '@trpc/server';
+import { User, Todo } from './db';    
  
 /**
  * Initialization of tRPC backend
  * Should be done only once per backend!
  */
-const t = initTRPC.create();
+
+const t = initTRPC.context<{db: {User: typeof User, Todo: typeof Todo}; userId?:string}>().create();
  
 /**
  * Export reusable router and procedure helpers
@@ -12,3 +14,4 @@ const t = initTRPC.create();
  */
 export const router = t.router;
 export const publicProcedure = t.procedure;
+export const middleware = t.middleware;
